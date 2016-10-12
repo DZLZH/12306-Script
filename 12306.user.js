3dc89b8f877e2d9fa6baa61b25bfb14cea7ff524
DZLZH <dzlzh@null.net>
2016-10-12 04:47:51 +0200

    12306.user.js init

---
+++ 12306.user.js
@@ -0,0 +1,5 @@
+//https://kyfw.12306.cn/otn/leftTicket/init
+//https://kyfw.12306.cn/otn/resources/js/query/train_list.js
+//https://kyfw.12306.cn/otn/leftTicket/log?leftTicketDTO.train_date=2016-12-10&leftTicketDTO.from_station=BXP&leftTicketDTO.to_station=HPP&purpose_codes=ADULT
+//https://kyfw.12306.cn/otn/leftTicket/queryT?leftTicketDTO.train_date=2016-12-10&leftTicketDTO.from_station=BXP&leftTicketDTO.to_station=HPP&purpose_codes=ADULT
+//https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=240000D90305&from_station_telecode=BXP&to_station_telecode=HPP&depart_date=2016-12-10
